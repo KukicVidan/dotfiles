a26654f1508545d94263f49ae7ad032ab541d901
Vidan <kukicvidan@gmail.com>
2025-04-11 01:02:42 +0200

    nvim tree background color

--- nvim/.config/nvim/lua/appearance.lua
+++ nvim/.config/nvim/lua/appearance.lua
@@ -1,42 +1,49 @@
 -- appearance.lua
 vim.cmd("colorscheme gruvbox-baby")
 
-require('lualine').setup {
-    options = {
-        theme = 'codedark',
-        component_separators = { left = '|', right = '|'},
-        section_separators = { left = '', right = ''},
-        disabled_filetypes = {
-            statusline = {'toggleterm'},
-            winbar = {},
-        },
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 100,
-            tabline = 100,
-            winbar = 100,
-        }
-    },
-    sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {{'filename', path = 1}},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-    },
-    inactive_sections = {
-        lualine_a = {'fileformat', {'filename', path = 1}, 'filetype'},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
-}
+-- Set Nvim Tree background color
+vim.api.nvim_set_hl(0, 'NvimTreeNormal', { bg = '#070603' })
+-- Set Nvim Tree background color
+vim.api.nvim_set_hl(0, 'NvimTreeNormalNC', { bg = '#070603' })
 
+
+
+
+require('lualine').setup {
+	options = {
+		theme = 'codedark',
+		component_separators = { left = '|', right = '|' },
+		section_separators = { left = '', right = '' },
+		disabled_filetypes = {
+			statusline = { 'toggleterm' },
+			winbar = {},
+		},
+		always_divide_middle = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 100,
+			tabline = 100,
+			winbar = 100,
+		}
+	},
+	sections = {
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', 'diff', 'diagnostics' },
+		lualine_c = { { 'filename', path = 1 } },
+		lualine_x = { 'encoding', 'fileformat', 'filetype' },
+		lualine_y = { 'progress' },
+		lualine_z = { 'location' }
+	},
+	inactive_sections = {
+		lualine_a = { 'fileformat', { 'filename', path = 1 }, 'filetype' },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {}
+}
